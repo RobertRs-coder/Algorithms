2622aeae83b9c86aaf65ca7d9292b7775b4b5ffb
Roberto Rojo <rrojo.va@gmail.com>
2024-03-20 22:27:47 +0100

    TwoSum

---
+++ TwoSum.playground/Contents.swift
@@ -0,0 +1,55 @@
+///Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+//You can return the answer in any order.
+//
+// 
+//
+//Example 1:
+//
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+//Example 2:
+//
+//Input: nums = [3,2,4], target = 6
+//Output: [1,2]
+//Example 3:
+//
+//Input: nums = [3,3], target = 6
+//Output: [0,1]
+// 
+import Foundation
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        for i in 0..<nums.count {
+            for j in i+1..<nums.count {
+                if nums[j] == target - nums[i] {
+                    return [i, j]
+                }
+            }
+        }
+        return []
+    }
+    
+    
+    func twoSumHash(_ nums: [Int], _ target: Int) -> [Int] {
+        var dict = [Int: Int]()
+        
+        for (index, value) in nums.enumerated() {
+            if let addent = dict[value] {
+                return [addent, index]
+            } else {
+                dict[target - value] = index
+            }
+        }
+        
+        return []
+    }
+}
+
+let sampleArray = [1,2,3]
+ 
+print(Solution().twoSum(sampleArray, 3))
